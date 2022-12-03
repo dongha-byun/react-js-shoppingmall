827fd62ad5707a343ec6e2f8dcdb677c5df1d2e6
dongha-byun <byunsw4@naver.com>
2022-12-03 20:44:05 +0900

    Feat: 사용자 환경설정 수정

--- src/component/page/user/config/UserBasicConfig.jsx
+++ src/component/page/user/config/UserBasicConfig.jsx
@@ -1,53 +1,55 @@
 import React from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import styledComponents from "styled-components";
-import TextInput from "../../../ui/TextInput";
-
-const StyledHeader = styledComponents.div`
-    margin-bottom: 25px;
-`;
+import ProviderHeader from "../../../provide/component/ProviderHeader";
 
 const UserBasicConfigWrapper = styledComponents.div`
     padding: 20px;
 `;
 
-function UserBasicConfig(props){
+function UserBasicConfig(props){   
+    const editConfig = () => {
+        alert("사용자 설정 수정");
+    }
 
     return (
         <UserBasicConfigWrapper>
-            <StyledHeader>
-                <h3 className="display-inline">사용자 기본정보 설정</h3>
-                <Button 
-                    className="float-right" 
-                    variant="outline-primary" 
-                    onClick={()=>alert("edit info")}>수정</Button>
-            </StyledHeader>
-            <Row>
-                <Col mx={6}>
-                    <Form.Group className="mb-3" controlId="loginId">
-                        <Form.Label>아이디</Form.Label>
-                        <Form.Control 
-                            type="text"
-                            value="byunsw4"
-                            disabled
-                            readOnly
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="username">
-                        <Form.Label>이름</Form.Label>
-                        <Form.Control 
-                            type="text"
-                            value="변동하"
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="telNo">
-                        <Form.Label>연락처</Form.Label>
-                        <Form.Control 
-                            value="010-0000-0000"
-                        />
-                    </Form.Group>
-                </Col>
-            </Row>
+            <ProviderHeader 
+                title="사용자 기본정보 설정"
+                onClick={editConfig}
+                buttonMessage="수정"
+            />
+            <Form>
+                <Form.Group className="mb-3" controlId="loginId">
+                    <Form.Label>아이디</Form.Label>
+                    <Form.Control 
+                        type="text" value="byunsw4" disabled readOnly
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="username">
+                    <Form.Label>이름</Form.Label>
+                    <Form.Control type="text" value="변동하" />
+                </Form.Group>
+                <Form.Label>연락처</Form.Label> 
+                <Row>
+                    <Col xs={8}>
+                        <Form.Group className="mb-3" controlId="telNo">
+                            <Form.Control value="010-0000-0000" />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Button variant="primary" onClick={()=>alert("연락처 인증")} >연락처 인증</Button>
+                    </Col>
+                </Row>
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>비밀번호 변경</Form.Label>
+                    <Form.Control type="password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="passwordConfirm">
+                    <Form.Label>비밀번호 변경 확인</Form.Label>
+                    <Form.Control type="password" />
+                </Form.Group>
+            </Form>
         </UserBasicConfigWrapper>
     );
 }
--- src/component/page/user/config/UserConfigPage.jsx
+++ src/component/page/user/config/UserConfigPage.jsx
@@ -27,6 +27,7 @@ function UserConfigPage(){
                 onSelect={onSelect}
             >
                 <Tab eventKey="basic" title="기본정보 관리" />
+                <Tab eventKey="basket" title="장바구니" />
                 <Tab eventKey="delivery" title="배송지 관리" />
                 <Tab eventKey="payment" title="결제수단 관리" />
                 <Tab eventKey="buy" title="구매내역" />
